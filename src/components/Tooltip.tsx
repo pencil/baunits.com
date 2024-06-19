9e1bda08953e242974d6dbf098e5e6c7c39ab31a
Nils Caspar <nils@smartinary.com>
2024-06-19 08:09:21 -0700

    Oh, hover works on mobile

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -9,7 +9,7 @@ export default function TextTooltip({ children, tooltip }: Props) {
     <button className="group relative inline-block">
       {children}
       {tooltip ? (
-        <span className="text-xs invisible group-hover:visible group-focus:visible opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition bg-slate-200 dark:bg-slate-600 dark:text-white p-1 rounded absolute top-full mt-2 whitespace-nowrap z-10 -translate-x-1/2 left-1/2">
+        <span className="text-xs invisible group-hover:visible opacity-0 group-hover:opacity-100 transition bg-slate-200 dark:bg-slate-600 dark:text-white p-1 rounded absolute top-full mt-2 whitespace-nowrap z-50 -translate-x-1/2 left-1/2">
           {tooltip}
         </span>
       ) : null}
