76cb5cf4131b6f4934d1c080e72daf1969bd21df
Nils Caspar <nils@smartinary.com>
2024-11-03 14:59:57 -0800

    Fix build

--- src/app/changelog/page.tsx
+++ src/app/changelog/page.tsx
@@ -25,44 +25,49 @@ const renderUpdatedUnits = (changes: (typeof changelog)[0]["changes"]) => {
     <>
       <h3 className="text-lg font-semibold">Updated Units</h3>
       <ul className="list-disc list-inside pl-2 md:pl-4">
-        {updatedUnits.map((unit) => (
-          <li key={unit.after.slug}>
-            <span className="font-semibold">{unit.after.name}</span>
-            <ul className="list-disc list-inside ml-4">
-              {Object.entries(unit.after).map(([key, value]) => {
-                if (
-                  key === "slug" ||
-                  key === "name" ||
-                  key === "ability" ||
-                  key === "index"
-                ) {
-                  return null;
-                }
-                const valueBefore = unit.before[key as keyof typeof unit.after];
-                if (valueBefore == value) {
-                  return null;
-                }
-                if (
-                  (typeof value !== "string" &&
-                    typeof value !== "number" &&
-                    typeof value !== "boolean") ||
-                  (typeof valueBefore !== "string" &&
-                    typeof valueBefore !== "number" &&
-                    typeof valueBefore !== "boolean")
-                ) {
-                  return null;
-                }
+        {updatedUnits.map(
+          (unit) =>
+            unit.after &&
+            unit.before && (
+              <li key={unit.after.slug}>
+                <span className="font-semibold">{unit.after.name}</span>
+                <ul className="list-disc list-inside ml-4">
+                  {Object.entries(unit.after).map(([key, value]) => {
+                    if (
+                      key === "slug" ||
+                      key === "name" ||
+                      key === "ability" ||
+                      key === "index"
+                    ) {
+                      return null;
+                    }
+                    const valueBefore =
+                      unit.before[key as keyof typeof unit.after];
+                    if (valueBefore == value) {
+                      return null;
+                    }
+                    if (
+                      (typeof value !== "string" &&
+                        typeof value !== "number" &&
+                        typeof value !== "boolean") ||
+                      (typeof valueBefore !== "string" &&
+                        typeof valueBefore !== "number" &&
+                        typeof valueBefore !== "boolean")
+                    ) {
+                      return null;
+                    }
 
-                return (
-                  <li key={key}>
-                    {beautifyKey(key)}:{" "}
-                    {valueBefore === null ? "?" : valueBefore} → {value}
-                  </li>
-                );
-              })}
-            </ul>
-          </li>
-        ))}
+                    return (
+                      <li key={key}>
+                        {beautifyKey(key)}:{" "}
+                        {valueBefore === null ? "?" : valueBefore} → {value}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </li>
+            ),
+        )}
       </ul>
     </>
   );
@@ -78,9 +83,10 @@ const renderNewUnits = (changes: (typeof changelog)[0]["changes"]) => {
     <>
       <h3 className="text-lg font-semibold">New Units</h3>
       <ul className="list-disc list-inside pl-2 md:pl-4">
-        {newUnits.map((unit) => (
-          <li key={unit.after.slug}>{unit.after.name}</li>
-        ))}
+        {newUnits.map(
+          (unit) =>
+            unit.after && <li key={unit.after.slug}>{unit.after.name}</li>,
+        )}
       </ul>
     </>
   );
@@ -96,9 +102,10 @@ const renderRemovedUnits = (changes: (typeof changelog)[0]["changes"]) => {
     <>
       <h3 className="text-lg font-semibold">Removed Units</h3>
       <ul className="list-disc list-inside pl-2 md:pl-4">
-        {removedUnits.map((unit) => (
-          <li key={unit.before.slug}>{unit.before.name}</li>
-        ))}
+        {removedUnits.map(
+          (unit) =>
+            unit.before && <li key={unit.before.slug}>{unit.before.name}</li>,
+        )}
       </ul>
     </>
   );
